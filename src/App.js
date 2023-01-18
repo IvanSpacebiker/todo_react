ac56d55401ae2157871c0a48a1c8b4995579b946
IvanSpacebiker <kii1234@yandex.ru>
2023-01-18 15:28:58 +0300

    Context menu

--- src/App.js
+++ src/App.js
@@ -1,84 +1,169 @@
-import React from "react";
-import getUniqueId from "uniqid";
-// Components
-import Input from "./components/Input";
-import Task from "./components/Task";
-// Styles
-import "./styles/App.css";
+import React from 'react';
+import './App.css';
 
-export default class App extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
+    class ToDo extends React.Component {
+        constructor(props) {
+            super(props);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleAddClick = this.handleAddClick.bind(this);
-        this.handleDeleteClick = this.handleDeleteClick.bind(this);
-        this.handleKey = this.handleKey.bind(this);
-
-        this.state = {
-            tasks      : [],
-            inputValue : "",
-        };
-    }
+            this.handleChange = this.handleChange.bind(this);
+            this.handleAddClick = this.handleAddClick.bind(this);
+            this.handleDeleteClick = this.handleDeleteClick.bind(this);
+            this.handleKey = this.handleKey.bind(this);
 
 
-    handleAddClick()
-    {
-        if (this.state.inputValue !== "")
-        {
-            const key = getUniqueId();
-            const task = <Task
-                key={key}
-                id={key}
-                value={this.state.inputValue}
-                onClick={this.handleDeleteClick}
-            />;
-            this.state.tasks.push(task);
-            this.setState({ inputValue: "" });
+            this.state = {
+                tasks: [],
+                input_value: "",
+            }
         }
-    }
 
-    handleKey(e)
-    {
-        if (e.key === "Enter")
-        {
-            this.handleAddClick();
-        }
-    }
 
-    handleDeleteClick(e)
-    {
-        const index = this.state.tasks.findIndex((task) => e.target.value === task.props.id);
-        const copy = this.state.tasks;
-        copy.splice(index, 1);
-        this.setState({ tasks: copy });
-    }
 
-    handleChange(e)
-    {
-        this.setState({ inputValue: e.target.value });
-    }
-
-    render()
-    {
-        return (
-            <div className="todo">
-                <h2 className="title">TO-OD</h2>
-                <Input
-                    onClick={this.handleAddClick}
-                    onChange={this.handleChange}
-                    onKeyDown={this.handleKey}
-                    value={this.state.inputValue}
+        handleAddClick(){
+            if(this.state.input_value !== ""){
+                const task = <Task
+                    key={Date.now()}
+                    id={Date.now()}
+                    value={this.state.input_value}
+                    onClick={this.handleDeleteClick}
                 />
-                <div className="list-field">
+                this.state.tasks.push(task)
+                this.setState({input_value: ""})
+            }
+        }
+
+        handleKey(e){
+            if (e.key === "Enter"){
+                this.handleAddClick()
+            }
+        }
+
+        handleDeleteClick(e){
+            console.log(e.target)
+            const index = this.state.tasks.findIndex(task => e.target.value === task.props.id)
+            const copy = this.state.tasks
+            copy.splice(index, 1)
+            this.setState({tasks: copy})
+        }
+
+        handleChange(e){
+            this.setState({input_value: e.target.value})
+        }
+
+        render() {
+            return (
+                <div className="todo">
+                    <p className="title">TO-OD</p>
+                    <Input
+                        onClick={this.handleAddClick}
+                        onChange={this.handleChange}
+                        onKeyPress={this.handleKey}
+                        value={this.state.input_value}
+                    />
+                    <div className="list-field">
+                        <ul>
+                            {this.state.tasks}
+                        </ul>
+                    </div>
+
+                </div>
+            );
+        }
+    }
+
+    class Input extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+
+        render() {
+            return (
+                <div className="add-field">
+                    <input
+                        className="add-input"
+                        value={this.props.value}
+                        onChange={this.props.onChange}
+                        onKeyPress={this.props.onKeyPress}
+                    />
+                    <button
+                        type="submit"
+                        onClick={() => this.props.onClick()}
+                        className="add-button"
+                    >+</button>
+                </div>
+            );
+        }
+    }
+
+    class Task extends React.Component{
+        constructor(props) {
+            super(props);
+            this.handleContext = this.handleContext.bind(this);
+            this.state = {
+                isDone: false,
+                value: this.props.value,
+                contextMenu: false
+            }
+        }
+
+        handleContext(e){
+            e.preventDefault()
+            let x = e.clientX + "px";
+            let y = e.clientY + "px";
+            document.documentElement.style.setProperty('--mouse-x', x);
+            document.documentElement.style.setProperty('--mouse-y', y);
+            this.setState({contextMenu: !this.state.contextMenu})
+
+        }
+
+        render() {
+            return (
+                <li
+                    className="list-item"
+                    onContextMenu={this.handleContext}
+                >
+                    <p
+                        className={this.state.isDone ? "task-name done" : "task-name"}
+                        title={this.state.value}
+                    >
+                        {this.state.value}
+                    </p>
+
+                    <ContextMenu
+                        isVisible = {this.state.contextMenu}
+                        isDone = {this.state.isDone ? "Undone" : "Done"}
+                        doneClick = {() => this.setState({isDone: !this.state.isDone, contextMenu: false})}
+                        deleteClick = {this.props.onClick}
+                    />
+                </li>
+            );
+        }
+    }
+
+    // context menu instead of checker and "x" button
+    class ContextMenu extends React.Component{
+        constructor(props) {
+            super(props);
+        }
+
+        render() {
+            return (
+                <div
+                    className="context-menu"
+                    hidden={!this.props.isVisible}
+                >
                     <ul>
-                        {this.state.tasks}
+                        <li
+                            onClick={this.props.doneClick}
+                        >{this.props.isDone}</li>
+                        <li
+                            onClick={this.props.deleteClick}
+                        >Delete</li>
                     </ul>
                 </div>
-
-            </div>
-        );
+            )
+        }
     }
-}
+
+
+export default ToDo;
